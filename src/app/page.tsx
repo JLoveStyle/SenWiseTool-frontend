aa88c25b154e7016e0ceaf3eab5a1947fdc4ccb8
Café TECH <cafetech.community@gmail.com>
2024-08-10 14:46:08 +0100

    simple updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { Layout } from "@/components/templates/layout";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
-      <p>hello world</p>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/images/logo.png"
+            height={150}
+            width={150}
+            alt="SenWiseTool"
+          />
+          <h1 className="text-3xl font-extrabold text-gray-700">
+            hello SENWISETOOL{" "}
+          </h1>
+        </div>
+      </div>
     </Layout>
   );
 }
