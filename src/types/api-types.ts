d4ca3ea63ea36515c14432ace34f709405ae1878
frckbrice <maebrie2017@gmail.com>
2024-09-03 17:39:05 +0100

    pushing update on creating companies and types

--- src/types/api-types.ts
+++ src/types/api-types.ts
@@ -21,4 +21,103 @@ export type PlanResultType = {
     status: number;
     data: PricePlanType[] | PricePlanType;
     message: string;
+    total?: number,
+    page?: number,
+    perPage?: number,
+    totalPages?: number,
+}
+
+export type ProjectType = {
+    id?: string;
+    type: "INITIAL_INSPECTION" | "EXTERNAL_INSPECTION" | "AUTO_EVALUATION" | "MAPPING";
+    company_id: string;
+    title: string;
+    description: string;
+    sector_activity: string;
+    country: string;
+    status: "ACTIVE" | "DELETED" | "ARCHIVED" | "DRAFT";
+    start_date: string;
+    end_date: string;
+    project_structure: JSON;
+    archived: boolean;
+    draft: boolean;
+
+    archived_at: string;
+    draft_at: string;
+    created_at: string;
+    deleted_at: string;
+    slug: string;
+    updated_at: string;
+}
+
+export type CompanyType = {
+    id: string,
+    name: string,
+    status: "ACTIVE" | "INACTIVE" | "EXPIRED",
+    slug: string,
+    country: string,
+    email: string,
+    logo: string,
+    payment_mode: string,
+    company_paypal_email: string,
+    paypal_id: string,
+    sector_of_activity: string,
+    website: string,
+    address: string,
+    phone_number: string,
+    city: string,
+    description: string,
+    created_at: string,
+    updated_at: string
+}
+
+
+
+export type UserType = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    companyId?: string;
+    company_id: string;
+    created_at: Date;
+    famer_attached_contract_url: string | null;
+    first_name: string;
+    last_name: string | null;
+    phone_number: string | null;
+    profileUrls: string | null;
+    role: "ADG" | "PDG" | "AUDITOR"
+    status: "ACTIVE" | "INACTIVE" | "BANNED";
+    updated_at: Date;
+    username: string | null;
+}
+
+export type TrainingType = {
+    company_id: string;
+    created_at: Date;
+    end_date: Date;
+    slug: string;
+    id: string;
+    location: string;
+    modules: string[];
+    report_url: string;
+    start_date: Date;
+    title: string;
+    trainer_proof_of_competency: string[];
+    training_attendance_sheet_url: string[];
+    training_picture_url: string[];
+    updated_at: Date;
+}
+
+export type Training_session = {
+    created_at: Date;
+    email: string;
+    first_name: string;
+    id: string;
+    last_name: string;
+    phone: string;
+    role: string;
+    signature: string;
+    training_id: string;
+    updated_at: Date;
 }
