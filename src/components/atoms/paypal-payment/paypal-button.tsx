a8c45583d1b43c050ccf0620491956c3ad89a1fe
frckbrice <maebrie2017@gmail.com>
2024-09-05 13:52:18 +0100

    rebasig to main

--- src/components/atoms/paypal-payment/paypal-button.tsx
+++ src/components/atoms/paypal-payment/paypal-button.tsx
@@ -9,48 +9,35 @@ import {
     SCRIPT_LOADING_STATE
 } from "@paypal/react-paypal-js";
 import { PricePlanType } from "@/types/api-types";
-<<<<<<< HEAD
 import { toast } from "sonner";
-=======
 import { usePriceStore } from "@/lib/stores/price-store";
->>>>>>> 9cea6f8 (fixing conflicts)
 
 const ButtonWrapper = ({ type }: { type: string, }) => {
     const [{ options }, dispatch] = usePayPalScriptReducer();
 
     // call the price store and init the plan
 
-    const price_plans = usePriceStore((state) => state.price_plan);
-    const plan_id = price_plans ? price_plans?.id : null;
+    const plan = usePriceStore((state) => state.price_plan);
+    const plan_id = plan ? plan?.id : null;
 
-    console.log("price plan: ", price_plans);
+    console.log("price plan: ", plan);
 
     // this is for mock only!!!!!!!!!!!!!!!!!!!
     const public_url = process.env.NEXT_PUBLIC_PUBLIC_URL;
     const server_url = process.env.NEXT_PUBLIC_SERVER_API_URL
     useEffect(() => {
         dispatch({
-<<<<<<< HEAD
-            // type: 'resetOptions' as DISPATCH_ACTION.RESET_OPTIONS,
-            type: 'setLoadingStatus' as DISPATCH_ACTION.LOADING_STATUS,
-            value: {
-                state: 'pending' as SCRIPT_LOADING_STATE.PENDING,
-                message: 'Loading...'
-            }
-=======
             type: 'setLoadingStatus' as DISPATCH_ACTION.LOADING_STATUS,
             value: {
                 message: 'PayPal is Initializing...',
                 state: 'pending' as SCRIPT_LOADING_STATE.PENDING
             },
->>>>>>> 9cea6f8 (fixing conflicts)
         });
     }, [type]);
 
     // TODO: handle the correct date starting for a plan.
     // to set the starting date.
     let now = new Date();
-    const plan_id = plan?.id;
     return (<PayPalButtons
         createSubscription={(data, actions) => {
             return actions.subscription
