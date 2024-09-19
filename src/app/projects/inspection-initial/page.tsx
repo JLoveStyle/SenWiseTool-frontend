43b2e71f0aeeebf0afdae2dd56dc9bf2ddb4cea2
Café TECH <cafetech.community@gmail.com>
2024-09-19 10:26:45 +0100

    simple update

--- src/app/projects/inspection-initial/page.tsx
+++ src/app/projects/inspection-initial/page.tsx
@@ -1,20 +1,21 @@
 "use client";
-import ProjectDisplay from "@/components/organisms/projectsDisplay";
 import LayoutDashboard from "@/components/organisms/layoutDashboard";
-import React, { useEffect } from "react";
+import ProjectDisplay from "@/components/organisms/projectsDisplay";
+import { useEffect } from "react";
 
 type Props = {};
 
 export default function Home({}: Props) {
-
-  // Fetch all projects with type ["INITIAL_INSPECTION"] and pass it as props to Layout
+  // Fetch all projects with type ["INITIAL_INSPECTION"] and pass it as props to Layout...
   useEffect(() => {
     console.log("initial_inspection");
-    
   }, []);
 
   return (
-    <LayoutDashboard projectsPerType={[]} typeOfProject={["INITIAL_INSPECTION"]}>
+    <LayoutDashboard
+      projectsPerType={[]}
+      typeOfProject={["INITIAL_INSPECTION"]}
+    >
       <ProjectDisplay projects={[]} />
     </LayoutDashboard>
   );
