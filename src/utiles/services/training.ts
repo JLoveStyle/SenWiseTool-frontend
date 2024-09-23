a7ee12387fec1055cc1c1538f1875c16e54d5378
Café TECH <cafetech.community@gmail.com>
2024-09-23 20:52:26 +0100

    fix updating training server response

--- src/utiles/services/training.ts
+++ src/utiles/services/training.ts
@@ -1,7 +1,7 @@
-"use server"
+"use server";
 import { Route } from "@/lib/route";
 import { ApiDataResponse, TrainingType } from "@/types/api-types";
-import { DBTrainingProps, } from "@/types/formData";
+import { DBTrainingProps } from "@/types/formData";
 import {
   mutateApiData,
   mutateDelApiData,
@@ -12,20 +12,20 @@ import { fetchApiData } from "./queries";
 // import { LOCAL_STORAGE } from "./storage";
 
 export const db_create_training = async (data: DBTrainingProps) => {
-
   return mutateApiData(Route.training, data)
     .then((response) => {
       if (response.status === 201)
         return {
           message: "Training created successfully ",
           response: response.data,
-          status: "success"
+          status: "success",
         };
-      else return {
-        message: "Updating training failed",
-        code: (response?.status as number) || 500,
-        status: "error"
-      };
+      else
+        return {
+          message: "Creating training failed",
+          code: (response?.status as number) || 500,
+          status: "error",
+        };
     })
     .catch((error) => {
       return { response: error, status: "error" };
@@ -33,51 +33,53 @@ export const db_create_training = async (data: DBTrainingProps) => {
 };
 
 export const db_update_training = async (data: DBTrainingProps, id: string) => {
-
-  return mutateUpApiData<ApiDataResponse<TrainingType>>(Route.training, data, id)
+  return mutateUpApiData<ApiDataResponse<TrainingType>>(
+    Route.training,
+    data,
+    id
+  )
     .then((response) => {
-      if (typeof response != 'undefined' && response?.status === 204)
-        return { data: response.message };
-      return {
-        error: {
-          message: "Updating training failed",
+      if (typeof response != "undefined" && response?.status === 204)
+        return {
+          message: "Training updated successfully ",
+          response: response.data,
+          status: "success",
+        };
+      else
+        return {
+          message: "Creating training failed",
           code: (response?.status as number) || 500,
-        },
-      };
+          status: "error",
+        };
     })
     .catch((error) => {
-      return {
-        error: {
-          message: (error as Error).message || "Erreur inconnue",
-          code: (error as any).code || 500,
-        },
-      };
+      return { response: error, status: "error" };
     });
 };
 
 export const db_get_trainings = async (companyId?: string) => {
   const db = new ApiCall();
 
-  return await fetchApiData<ApiDataResponse<TrainingType[] | TrainingType>>(Route.training, companyId)
+  return await fetchApiData<ApiDataResponse<TrainingType[] | TrainingType>>(
+    Route.training,
+    companyId
+  )
     .then((response) => {
-      if (typeof response != 'undefined') {
-        return response.data
+      if (typeof response != "undefined") {
+        return response.data;
       }
-
     })
     .catch((error) => {
       console.error("Erreur lors de l'envoi des données :", error);
-      if (!companyId)
-        return [] as TrainingType[];
-      return null
+      if (!companyId) return [] as TrainingType[];
+      return null;
     });
 };
 
 export const db_delete_training = async (id: string) => {
-
   return mutateDelApiData<ApiDataResponse<TrainingType>>(Route.training, id)
     .then((response) => {
-      if (typeof response != 'undefined' && response?.status === 204)
+      if (typeof response != "undefined" && response?.status === 204)
         return { data: response.message };
       return {
         error: {
