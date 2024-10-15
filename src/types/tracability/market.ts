053da8c2a87af38970cdceadf79d7320338fd453
GaelTikeng <Gaelinho@yahoo.fr>
2024-10-15 17:53:31 +0100

    created an api response the market table

--- src/types/tracability/market.ts
+++ src/types/tracability/market.ts
@@ -19,21 +19,18 @@ export type DBMarketProps = {
   price_of_day: number;
   start_date: Date | string;
   end_date: Date | string;
-  // market_number: number;
   description?: string;
-  // location: string
-  // type_of_market?: $Enums.MarketType
-  // bordereau_vente_url: string
-  // bon_entree_magazin_url: string
-  status?: "WAITING" | "IN_PROGRESS" | "CLOSED";
-  // code?: string | null
-  // product_quantity: number
+  location: string
+  type_of_market?: 'COCOA' | 'COFFEE' | 'BANANA' | 'WOOD' | 'OTHER'
+  status?: "OPEN" | "CLOSED";
+  code?: string | null
+  product_quantity: number
   created_at?: Date | string;
   updated_at?: Date | string;
   campaign_id: string;
   company_id?: string;
-  // company: CompanyCreateNestedOneWithoutMarketsInput
-  // market_audit?: Market_auditCreateNestedManyWithoutMarketInput
-  // transaction?: TransactionCreateNestedManyWithoutMarketInput
-  // receipts?: ReceiptCreateNestedManyWithoutMarketInput
+  company: string
+  market_audit: [] // market_audit object[]: to be defined
+  transaction: [] // transaction object[]: to be defined 
+  receipts?: [] // receipt object[]: to be defined
 };
