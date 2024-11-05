eb4875dcbfa2a80ffa4337eafb1bf95e310b4b10
JLove <jlove.livestyle@gmail.com>
2024-11-05 10:34:32 +0100

    update 2

--- src/app/dashboard/agriculture/[id]/generateStaticParams.ts
+++ src/app/dashboard/agriculture/[id]/generateStaticParams.ts
@@ -1,10 +1,18 @@
+import { Route } from "@/lib/route";
 import { ActivityProps } from "@/types/activity";
-import { LOCAL_STORAGE } from "@/utiles/services/storage";
+import { fetchApiData } from "@/utiles/services/queries";
 
 // Fonction pour générer les paramètres statiques
 export async function generateStaticParams() {
-  const activities = await LOCAL_STORAGE.get("agricultures");
+  const activities = await fetchApiData(Route.agricultureRequest, "");
   return activities.map((activity: ActivityProps) => ({
     id: activity.id?.toString(),
   }));
 }
+
+// export async function generateStaticParams() {
+//   const activities = ["first", "second", "tirth"]; //await fetchApiData(Route.agricultureRequest, "");
+//   return activities.map((activity: string) => ({
+//     id: activity,
+//   }));
+// }
