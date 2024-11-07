c7cc608e58fb7cac0ccefbcadea339ace2488399
JLoveStyle <141050956+JLoveStyle@users.noreply.github.com>
2024-11-07 09:52:52 +0100

    Update constants.ts

--- src/utiles/services/constants.ts
+++ src/utiles/services/constants.ts
@@ -6,8 +6,8 @@ import { receiptStatData } from "../tracability.const/statistics";
 import { IJsonSheet } from "json-as-xlsx";
 
 export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL;
-// export const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL;
-export const API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL;
+// export const API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
 
 // BUSINESS ACTIVITIES
 export const businessActivity: string[] = [
