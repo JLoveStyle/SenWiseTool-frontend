482b4fd404cb0aa4af1c53aef2374f17ea28b298
frckbrice <maebrie2017@gmail.com>
2025-03-22 23:28:21 +0100

    update the path to base url and server url

--- src/utiles/services/constants.ts
+++ src/utiles/services/constants.ts
@@ -5,11 +5,12 @@ import { Archive, FilePenLine, Rocket } from "lucide-react";
 import { receiptStatData } from "../tracability.const/statistics";
 
 // export const BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL || 'http://localhost:3000';
-export const BASE_URL = "https://wsflnurjpk6y4rfu66vtm2uz4q.srv.us";
+// export const BASE_URL = "https://wsflnurjpk6y4rfu66vtm2uz4q.srv.us";
+export const BASE_URL = "https://senwisetool.com";
 // process.env.NEXT_PUBLIC_SENWISETOOL_URL;
 
-// export const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL;
-export const API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL;
+// export const API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
 
 export const PAYMENT_API_URL = process.env.NEXT_PUBLIC_PAYMENT_API_URL;
 
